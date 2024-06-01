920a9f1b49fae315627fe1225be6114859754847
arafatGithubCode <md489288@gmail.com>
2024-06-01 15:03:38 +0600

    create sale section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../components/banner/Banner";
 import BannerBottom from "../components/banner/BannerBottom";
+import Sale from "../components/home/sale/Sale";
 
 const Home = () => {
   return (
     <div className="w-full mx-auto px-2">
       <Banner />
       <BannerBottom />
+      <div className="">
+        <Sale />
+      </div>
     </div>
   );
 };
