d59d87d91ac9333b66208db8f354dfb2bdc99f0c
arafatGithubCode <md489288@gmail.com>
2024-06-05 08:14:35 +0600

    create cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,36 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 import ItemCart from "./ItemCart";
+import { resetCart } from "../../redux/shopLoverSlice";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const products = useSelector((state) => state.shopLoverReducer.products);
+  const dispatch = useDispatch();
+
+  const [totalAmt, setTotalAmt] = useState("");
+  const [shippingCharge, setShippingCharge] = useState("");
+
+  useEffect(() => {
+    let price = 0;
+
+    products.map((item) => {
+      price += item.price * item.quantity;
+      return price;
+    });
+    setTotalAmt(price);
+  }, [products]);
+
+  useEffect(() => {
+    if (totalAmt <= 200) {
+      setShippingCharge(30);
+    } else if (totalAmt <= 400) {
+      setShippingCharge(25);
+    } else if (totalAmt > 401) {
+      setShippingCharge(20);
+    }
+  }, [totalAmt]);
 
   return (
     <div className="max-w-container mx-auto px-4">
@@ -22,6 +49,60 @@ const Cart = () => {
                 <ItemCart item={item} />
               </div>
             ))}
+          </div>
+
+          <button
+            onClick={() => dispatch(resetCart())}
+            className="py-2 px-10 bg-red-500 text-white font-semibold uppercase mb-4 hover:bg-red-700 duration-300"
+          >
+            reset cart
+          </button>
+
+          <div className="flex flex-col mdl:flex-row justify-between border py-4 px-4 items-center gap-2 mdl:gap-0">
+            <div className="flex items-center gap-4">
+              <input
+                className="w-44 mdl:w-52 h-8 px-4 border text-primeColor text-sm outline-none border-gray-400"
+                type="text"
+                placeholder="Coupon Number"
+              />
+              <p className="text-sm mdl:text-base font-semibold">
+                Apply Coupon
+              </p>
+            </div>
+            <p className="text-lg font-semibold">Update Cart</p>
+          </div>
+
+          <div className="max-w-7xl gap-4 flex justify-end mt-4">
+            <div className="w-96 flex flex-col gap-4">
+              <h1 className="text-2xl font-semibold text-right">Cart totals</h1>
+              <div>
+                <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
+                  Subtotal
+                  <span className="font-semibold tracking-wide font-titleFont">
+                    ${totalAmt}
+                  </span>
+                </p>
+                <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
+                  Shipping Charge
+                  <span className="font-semibold tracking-wide font-titleFont">
+                    ${shippingCharge}
+                  </span>
+                </p>
+                <p className="flex items-center justify-between border-[1px] border-gray-400 py-1.5 text-lg px-4 font-medium">
+                  Total
+                  <span className="font-bold tracking-wide text-lg font-titleFont">
+                    ${totalAmt + shippingCharge}
+                  </span>
+                </p>
+              </div>
+              <div className="flex justify-end">
+                <Link to="/paymentgateway">
+                  <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
+                    Proceed to Checkout
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       ) : (
