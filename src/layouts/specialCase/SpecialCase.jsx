aef9684abcdf6dae7fce69d5a8d6387ac4663127
arafatGithubCode <md489288@gmail.com>
2024-06-05 16:28:38 +0600

    complete the project UI

--- src/layouts/specialCase/SpecialCase.jsx
+++ src/layouts/specialCase/SpecialCase.jsx
@@ -1,8 +1,11 @@
 import { MdSwitchAccount } from "react-icons/md";
 import { RiShoppingCart2Fill } from "react-icons/ri";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const SpecialCase = () => {
+  const products = useSelector((state) => state.shopLoverReducer.products);
+
   return (
     <div className="fixed top-52 right-5 z-50 sm:flex flex-col gap-2 hidden">
       <Link to="/signin">
@@ -14,16 +17,19 @@ const SpecialCase = () => {
           <p className="text-xs font-titleFont font-semibold">Profile</p>
         </div>
       </Link>
-      <Link to="/shop">
+      <Link to="/cart">
         <div className="bg-white w-16 h-[70px] rounded-lg flex flex-col justify-center items-center gap-1 cursor-pointer shadow-testShadow overflow-x-hidden group relative">
           <div className="flex justify-center items-center">
             <RiShoppingCart2Fill className="text-2xl -translate-x-12 group-hover:translate-x-3 transition duration-200 ease-in" />
             <RiShoppingCart2Fill className="text-2xl -translate-x-3 group-hover:translate-x-12 transition duration-200 ease-in" />
           </div>
           <p className="text-xs font-titleFont font-semibold">Buy Now</p>
-          <p className="absolute top-1 right-2 bg-primeColor text-white text-xs w-4 h-4 rounded-full flex items-center justify-center font-semibold">
-            5
-          </p>
+
+          {products.length > 0 && (
+            <p className="absolute top-1 right-2 bg-primeColor text-white text-xs w-4 h-4 rounded-full flex items-center justify-center font-semibold">
+              {products.length}
+            </p>
+          )}
         </div>
       </Link>
     </div>
